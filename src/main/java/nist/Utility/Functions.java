f2b57248923756729f051a74f72e9e2419ec7d8a
cmejia5486 <52473133+cmejia5486@users.noreply.github.com>
2025-02-10 08:38:15 -0500

    Update Functions.java

--- src/main/java/nist/Utility/Functions.java
+++ src/main/java/nist/Utility/Functions.java
@@ -16,35 +16,6 @@ import java.text.DecimalFormat;
  */
 public class Functions {
 
-    /** Path to JSON files containing CVE data for each year. */
-    public static final String FILE2002 = "JsonData/nvdcve-1.1-2002.json";
-    public static final String FILE2003 = "JsonData/nvdcve-1.1-2003.json";
-    public static final String FILE2004 = "JsonData/nvdcve-1.1-2004.json";
-    public static final String FILE2005 = "JsonData/nvdcve-1.1-2005.json";
-    public static final String FILE2006 = "JsonData/nvdcve-1.1-2006.json";
-    public static final String FILE2007 = "JsonData/nvdcve-1.1-2007.json";
-    public static final String FILE2008 = "JsonData/nvdcve-1.1-2008.json";
-    public static final String FILE2009 = "JsonData/nvdcve-1.1-2009.json";
-    public static final String FILE2010 = "JsonData/nvdcve-1.1-2010.json";
-    public static final String FILE2011 = "JsonData/nvdcve-1.1-2011.json";
-    public static final String FILE2012 = "JsonData/nvdcve-1.1-2012.json";
-    public static final String FILE2013 = "JsonData/nvdcve-1.1-2013.json";
-    public static final String FILE2014 = "JsonData/nvdcve-1.1-2014.json";
-    public static final String FILE2015 = "JsonData/nvdcve-1.1-2015.json";
-    public static final String FILE2016 = "JsonData/nvdcve-1.1-2016.json";
-    public static final String FILE2017 = "JsonData/nvdcve-1.1-2017.json";
-    public static final String FILE2018 = "JsonData/nvdcve-1.1-2018.json";
-    public static final String FILE2019 = "JsonData/nvdcve-1.1-2019.json";
-    public static final String FILE2020 = "JsonData/nvdcve-1.1-2020.json";
-    public static final String FILE2021 = "JsonData/nvdcve-1.1-2021.json";
-    public static final String FILE2022 = "JsonData/nvdcve-1.1-2022.json";
-    public static final String FILE2023 = "JsonData/nvdcve-1.1-2023.json";
-    public static final String FILE2024 = "JsonData/nvdcve-1.1-2024.json";
-    public static final String FILE2025 = "JsonData/nvdcve-1.1-2025.json";
-
-    /** Path to the consolidated JSON file containing total CVE data. */
-    public static final String FILETOTAL = "JsonData/Total.json";
-
     /**
      * Checks if a given string is null and returns an empty string instead.
      *
